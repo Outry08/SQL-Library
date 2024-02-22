c124b53c46f32f45ab3ffe7d21b66e43e361b51d
Ryan Mckinnon <outryvball04@gmail.com>
2024-02-22 17:42:01 -0500

    Fully implemented update function complete with multiple WHERE condition functionality.

--- sqllib.c
+++ sqllib.c
@@ -14,6 +14,42 @@ int main(int argc, char const* argv[]) {
     insert(&table, 3, "Integer Col", 44, "Decimal Col", 88.77, "Char Col", "WAHHLUIGI");
     printTable(table);
 
+    Where where1 = newWhere("Char Col", "==", "Luigi");
+    double newNum = 9999.999;
+
+
+    update(&table, "Decimal Col", &newNum, 1, where1);
+    printTable(table);
+
+    newNum = 0;
+    Where where2 = newWhere("Decimal Col", "==", &newNum);
+
+    int newInt = 1735;
+
+    update(&table, "Integer Col", &newInt, 2, where1, '&', where2);
+    printTable(table);
+
+    Where where3 = newWhere("Char Col", "==", "NULL");
+    // update(&table, "Char Col", "WAAAAARIOOOOOOO", 1, where3);
+    printTable(table);
+
+    insert(&table, 1, "Decimal Col", 444.0);
+
+    printTable(table);
+    newInt = 666;
+    update(&table, "Integer Col", &newInt, 3, where3, '&', where2, '|', where1);
+    printTable(table);
+
+    Where where4 = newWhere("Integer Col", "==", &newInt);
+    update(&table, "Char Col", "WAAAAARIOOOOOO", 2, where3, '&', where4);
+    printTable(table);
+
+    where3.comparison = "<>";
+
+    update(&table, "Char Col", "Peach", 1, where3);
+    printTable(table);
+
+
     return 0;
 }
 
@@ -32,7 +68,7 @@ Table create(char* tableName, int numCols, ...) {
 
     va_start(ap, numCols);
 
-    for (int i = 0; i < numCols; i++) {
+    for(int i = 0; i < numCols; i++) {
         table.cols[i].name = strdup(va_arg(ap, char*));
         table.cols[i].type = va_arg(ap, int);
     }
@@ -55,7 +91,7 @@ Table create(char* tableName, int numCols, ...) {
 */
 void insert(Table* table, int numValues, ...) {
 
-    if (numValues > table->numCols) {
+    if(numValues > table->numCols) {
         printf("Error: To many arguments provided. Insertion failed.\n");
         return;
     }
@@ -67,25 +103,25 @@ void insert(Table* table, int numValues, ...) {
 
     va_start(ap, numValues);
 
-    for (int i = 0; i < table->numCols; i++) {
+    for(int i = 0; i < table->numCols; i++) {
         table->cols[i].values = realloc(table->cols[i].values, sizeof(Column) * table->numRows);
-        if (table->cols[i].type == CHAR)
+        if(table->cols[i].type == CHAR)
             table->cols[i].values[table->numRows - 1].CHAR = "NULL";
-        else if (table->cols[i].type == INTEGER)
+        else if(table->cols[i].type == INTEGER)
             table->cols[i].values[table->numRows - 1].INTEGER = 0;
-        else if (table->cols[i].type == DECIMAL)
+        else if(table->cols[i].type == DECIMAL)
             table->cols[i].values[table->numRows - 1].DECIMAL = 0.0;
     }
 
-    for (int i = 0; i < numValues; i++) {
+    for(int i = 0; i < numValues; i++) {
         colName = strdup(va_arg(ap, char*));
         currCol = nameToCol(table, colName);
 
-        if (currCol.type == CHAR)
+        if(currCol.type == CHAR)
             currCol.values[table->numRows - 1].CHAR = va_arg(ap, char*);
-        else if (currCol.type == INTEGER)
+        else if(currCol.type == INTEGER)
             currCol.values[table->numRows - 1].INTEGER = va_arg(ap, int);
-        else if (currCol.type == DECIMAL)
+        else if(currCol.type == DECIMAL)
             currCol.values[table->numRows - 1].DECIMAL = va_arg(ap, double);
         else {
             printf("Error: Nonexistent column name, \"%s\", provided.\n", colName);
@@ -98,22 +134,82 @@ void insert(Table* table, int numValues, ...) {
     va_end(ap);
 }
 
-
-void update(Table* table, char* colName, void* newValue, ...) {
+/**
+ * Format: update(Table* table, char* colName, void* newValue, int numConds, Where where1, char connective1, Where where2, ...)
+ * (Accepted connectives being '&' or '|' meaning AND or OR)
+ * SQL Origin:
+ *  UPDATE tableName
+ *   SET colName = newValue
+ *   WHERE tableName.whereColName =, >, <, >=, <=, != whereValue
+*/
+void update(Table* table, char* colName, void* newValue, int numConds, ...) {
     va_list ap;
 
-    va_start(ap, newValue);
+    va_start(ap, numConds);
 
+    Column column = nameToCol(table, colName);
+    Where currentWhere;
+    char currConnect = '\0';
+
+    int toUpdate[table->numRows];
+    for(int i = 0; i < table->numRows; i++) {
+        toUpdate[i] = 1;
+    }
+
+    for(int i = 0; i < numConds; i++) {
+        currentWhere = va_arg(ap, Where);
+        for(int j = 0; j < table->numRows; j++) {
+            //Right now only stringing where statements together with AND, need OR to be incorporated.
+            if(currConnect == '&' || currConnect == 'a' || currConnect == 'A') {
+                if(toUpdate[j] == 1 && !compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue)) {
+                    toUpdate[j] = 0;
+                }
+            }
+            else if(currConnect == '|' || currConnect == 'o' || currConnect == 'O') {
+                if(toUpdate[j] == 1) {
+                    if(column.type == CHAR)
+                        column.values[j].CHAR = (char*)newValue;
+                    else if(column.type == INTEGER)
+                        column.values[j].INTEGER = *((int*)newValue);
+                    else if(column.type == DECIMAL)
+                        column.values[j].DECIMAL = *((double*)newValue);
+
+                    return;
+                }
+
+                if(toUpdate[j] == 0 && compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue)) {
+                    toUpdate[j] = 1;
+                }
+            }
+            else if(currConnect == '\0') {
+                if(!compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue))
+                    toUpdate[j] = 0;
+            }
+            else {
+                printf("Error: Invalid where connective '%c' provided into update function.", currConnect);
+            }
+            if(i == numConds - 1 && toUpdate[j] == 1) {
+                if(column.type == CHAR)
+                    column.values[j].CHAR = (char*)newValue;
+                else if(column.type == INTEGER)
+                    column.values[j].INTEGER = *((int*)newValue);
+                else if(column.type == DECIMAL)
+                    column.values[j].DECIMAL = *((double*)newValue);
+            }
+
+        }
+        if(i < numConds - 1) {
+            currConnect = va_arg(ap, int);
+        }
+    }
 
 }
 
 Column nameToCol(Table* table, char* colName) {
 
-    for (int i = 0; i < table->numCols; i++) {
-        if (strcmp(table->cols[i].name, colName) == 0) {
+    for(int i = 0; i < table->numCols; i++)
+        if(strcmp(table->cols[i].name, colName) == 0)
             return table->cols[i];
-        }
-    }
 
     Column errCol;
     errCol.name = "COL NOT FOUND";
@@ -123,22 +219,108 @@ Column nameToCol(Table* table, char* colName) {
     return errCol;
 }
 
+int compare(Column column, int valIndex, char* comparison, void* value) {
+    if(strcmp(comparison, "=") == 0 || strcmp(comparison, "==") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) == 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER == *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL == *((double*)value);
+        else
+            return 0;
+    }
+    else if(strcmp(comparison, "<") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) < 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER < *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL < *((double*)value);
+        else
+            return 0;
+    }
+    else if(strcmp(comparison, "<=") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) <= 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER <= *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL <= *((double*)value);
+        else
+            return 0;
+    }
+    else if(strcmp(comparison, ">") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) > 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER > *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL > *((double*)value);
+        else
+            return 0;
+    }
+    else if(strcmp(comparison, ">=") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) >= 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER >= *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL >= *((double*)value);
+        else
+            return 0;
+    }
+    else if(strcmp(comparison, "<>") == 0 || strcmp(comparison, "!=") == 0) {
+        if(column.type == CHAR)
+            return strcmp(column.values[valIndex].CHAR, (char*)value) != 0;
+        else if(column.type == INTEGER)
+            return column.values[valIndex].INTEGER != *((int*)value);
+        else if(column.type == DECIMAL)
+            return column.values[valIndex].DECIMAL != *((double*)value);
+        else
+            return 0;
+    }
+    else {
+        printf("Error: Invalid comparison entered.\n");
+        return 0;
+    }
+
+}
+
+Where newWhere(char* searchColName, char* comparison, void* searchValue) {
+    Where newWhere;
+    newWhere.searchColName = strdup(searchColName);
+    newWhere.comparison = strdup(comparison);
+    newWhere.searchValue = malloc(sizeof(void*));
+    memcpy(newWhere.searchValue, searchValue, sizeof(searchValue));
+    // newWhere.searchValue = searchValue;
+
+    return newWhere;
+}
+
 void printTable(Table table) {
 
-    printf("\nTABLE: %s\nCOLUMNS:", table.name);
+    printf("\nTABLE:\n\t\t%s\n\nCOLUMNS:\n\t   ", table.name);
+    char printString[50];
 
-    for (int i = 0; i < table.numCols; i++)
-        printf("\t%s", table.cols[i].name);
+    for(int i = 0; i < table.numCols; i++)
+        printf("%-25s", table.cols[i].name);
 
-    for (int i = 0; i < table.numRows; i++) {
+    for(int i = 0; i < table.numRows; i++) {
         printf("\n\t");
-        for (int j = 0; j < table.numCols; j++) {
-            if (table.cols[j].type == CHAR)
-                printf("\t[ %s ]", table.cols[j].values[i].CHAR);
-            else if (table.cols[j].type == INTEGER)
-                printf("\t[ %d ]", table.cols[j].values[i].INTEGER);
-            else if (table.cols[j].type == DECIMAL)
-                printf("\t[ %.3lf ]", table.cols[j].values[i].DECIMAL);
+        for(int j = 0; j < table.numCols; j++) {
+            if(table.cols[j].type == CHAR) {
+                sprintf(printString, "    [ %s ]", table.cols[j].values[i].CHAR);
+                printf("%-25s", printString);
+            }
+            else if(table.cols[j].type == INTEGER) {
+                sprintf(printString, "    [ %d ]", table.cols[j].values[i].INTEGER);
+                printf("%-25s", printString);
+            }
+            else if(table.cols[j].type == DECIMAL) {
+                sprintf(printString, "    [ %.3lf ]", table.cols[j].values[i].DECIMAL);
+                printf("%-25s", printString);
+            }
         }
     }
 
--- sqllib.h
+++ sqllib.h
@@ -29,34 +29,36 @@ typedef struct {
 } Table;
 
 typedef struct {
-    char* column;
+    char* searchColName;
     char* comparison;
     void* searchValue;
 } Where;
 
-Table create(char* tableName, int numCols, ...);
+Table create(char* tableName, int numCols, ...); //Implemented
 //... --> char *col1Name, ...
 
-Table select(Column col, Table table, int distinct);
-Table selectWhere(Column col, Table table, int distinct, ...);
-//... --> Where where1, char* AND/OR, Where where2, ...
+Table select(char* colName, Table table, int distinct);
+Table selectWhere(char* colName, Table table, int distinct, int numConds, ...);
+//... --> Where where1, char &/|, Where where2, ...
 
-void insert(Table* table, int numValues, ...);
+void insert(Table* table, int numValues, ...); //Implemented
 //... --> char *col1Name, void *value1, ...
-void update(Table* table, char* colName, void* newValue, ...);
+void update(Table* table, char* colName, void* newValue, int numConds, ...); //Implemented
 //... --> Where where1, char* AND/OR, Where where2, ...
-void delete(Table* table, ...);
+void delete(Table* table, int numConds, ...);
 //... --> Where where1, char* AND/OR, Where where2, ...
 void deleteAll(Table* table);
 void freeTable(Table* table);
 
 void addColumn(Table* table, char* colName);
-Column nameToCol(Table* table, char* colName);
+Column nameToCol(Table* table, char* colName); //Implemented
 
-void printTable(Table table);
+void printTable(Table table); //Implemented
 void exportString(Table table);
 void exportSQL(Table table, char* filename);
 Table importTable(char* filename);
+int compare(Column column, int valIndex, char* comparison, void* value); //Implemented
+Where newWhere(char* searchColName, char* comparison, void* searchValue); //Implemented
 
 Table matrixToTable(void** matrix, char* tableName, char** colNames, int type);
 void matrixToSQL(void** matrix, int type, char* filename);
