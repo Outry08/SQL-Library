0374233c088b620ac55e8791b39756e7bfdc35cd
Ryan Mckinnon <outryvball04@gmail.com>
2024-02-26 22:31:37 -0500

    Made a struct for selecting. Reworked use of ellipses for update and delete, as well as made functions for easy creationg of lists of various datatypes. SELECT working for selecting multiple columns with multiple where conditions. Up next is added support for GROUP BY and ORDER BY.

--- sqllib.c
+++ sqllib.c
@@ -18,7 +18,7 @@ int main(int argc, char const* argv[]) {
     double newNum = 9999.999;
 
 
-    update(&table, "Decimal Col", &newNum, 1, where1);
+    update(&table, "Decimal Col", &newNum, 1, whereList(1, where1), connectiveList(0));
     printTable(table);
 
     newNum = 0;
@@ -26,40 +26,55 @@ int main(int argc, char const* argv[]) {
 
     int newInt = 1735;
 
-    update(&table, "Integer Col", &newInt, 2, where1, '&', where2);
+    update(&table, "Integer Col", &newInt, 2, whereList(2, where1, where2), connectiveList(1, '|'));
     printTable(table);
 
     Where where3 = newWhere("Char Col", "==", "NULL");
-    // update(&table, "Char Col", "WAAAAARIOOOOOOO", 1, where3);
+    update(&table, "Char Col", "WAAAAARIOOOOOOO", 1, whereList(1, where3), connectiveList(0));
     printTable(table);
 
     insert(&table, 1, "Decimal Col", 444.0);
 
     printTable(table);
     newInt = 666;
-    update(&table, "Integer Col", &newInt, 3, where3, '&', where2, '|', where1);
+
+    update(&table, "Integer Col", &newInt, 3, whereList(3, where3, where2, where1), connectiveList(2, '&', '|'));
     printTable(table);
 
     Where where4 = newWhere("Integer Col", "==", &newInt);
-    update(&table, "Char Col", "WAAAAARIOOOOOO", 2, where3, '&', where4);
+    update(&table, "Char Col", "WAAAAAAAAAAAAAAARIOOOOOO", 2, whereList(2, where3, where4), connectiveList(1, '&'));
     printTable(table);
 
-    // where3.comparison = "<>";
+    where3.comparison = "==";
 
-    // update(&table, "Char Col", "Peach", 1, where3);
-    // printTable(table);
+    update(&table, "Char Col", "Peach", 1, whereList(1, where3), connectiveList(0));
+    printTable(table);
 
     double dec = 444.0;
 
-    Where where5 = newWhere("Char Col", "==", "WAAAAARIOOOOOO");
+    Where where5 = newWhere("Char Col", "==", "WAAAAAAAAAAAAAAARIOOOOOO");
     Where where6 = newWhere("Decimal Col", ">", &dec);
     Where where7 = newWhere("Decimal Col", "==", &dec);
     Where where8 = newWhere("Integer Col", "==", &newNum);
     where1.comparison = "!=";
-    delete(&table, 7, where5, '|', where3, '&', where6, '|', where4, '|', where1, '&', where7, '&', where8);
+    delete(&table, 7, whereList(7, where5, where3, where6, where4, where1, where7, where8), connectiveList(6, '|', '&', '|', '|', '&', '&'));
+    where3.comparison = "!=";
+    delete(&table, 2, whereList(2, where5, where3), connectiveList(1, '&'));
 
     // deleteAll(&table);
     printTable(table);
+
+    printf("\n\n\n");
+    where3.searchValue = "Mario";
+
+    Select select1 = newSelect(2, nameList(2, "Decimal Col", "Char Col"), NULL, NULL, 0);
+
+    // where3.comparison = "==";
+    // where5.comparison = "!=";
+    // delete(&table, 2, whereList(2, where5, where3), connectiveList(1, '&'));
+    // printTable(table);
+
+    printTable(select(table, select1, 2, whereList(2, where5, where3), connectiveList(1, '|')));
 
     return 0;
 }
@@ -146,89 +161,77 @@ void insert(Table* table, int numValues, ...) {
 }
 
 /**
- * Format: update(Table* table, char* colName, void* newValue, int numConds, Where where1, char connective1, Where where2, ...)
+ * Format: update(Table* table, char* colName, void* newValue, int numWheres, Where where1, char connective1, Where where2, ...)
  * (Accepted connectives being '&' or '|' meaning AND or OR)
  * SQL Origin:
  *  UPDATE tableName
  *   SET colName = newValue
  *   WHERE tableName.whereColName =, >, <, >=, <=, != whereValue
 */
-void update(Table* table, char* colName, void* newValue, int numConds, ...) {
-    va_list ap;
-
-    va_start(ap, numConds);
+void update(Table* table, char* colName, void* newValue, int numWheres, Where* wheres, char* conns) {
 
     Column column = nameToCol(table, colName);
-    Where currentWhere;
-    char currConnect = '\0';
-
+    int* prevGoodJs;
+    int numPrevGoodJs;
     int toUpdate[table->numRows];
-    for(int i = 0; i < table->numRows; i++) {
+
+    for(int i = 0; i < table->numRows; i++)
         toUpdate[i] = 1;
-    }
 
-    for(int i = 0; i < numConds; i++) {
-        currentWhere = va_arg(ap, Where);
+    for(int i = 0; i < numWheres; i++) {
+        prevGoodJs = malloc(sizeof(int));
+        numPrevGoodJs = 0;
         for(int j = 0; j < table->numRows; j++) {
-            if(currConnect == '&' || currConnect == 'a' || currConnect == 'A') {
-                if(toUpdate[j] == 1 && !compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue)) {
-                    toUpdate[j] = 0;
-                }
-            }
-            else if(currConnect == '|' || currConnect == 'o' || currConnect == 'O') {
-                if(toUpdate[j] == 1) {
-                    if(column.type == CHAR)
-                        column.values[j].CHAR = (char*)newValue;
-                    else if(column.type == INTEGER)
-                        column.values[j].INTEGER = *((int*)newValue);
-                    else if(column.type == DECIMAL)
-                        column.values[j].DECIMAL = *((double*)newValue);
-
-                    return;
-                }
-
-                if(toUpdate[j] == 0 && compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue)) {
+            switch(conns[i]) {
+                case '&':
+                case 'a':
+                case 'A':
+                    if(toUpdate[j] == 1 && !compare(nameToCol(table, wheres[i].searchColName), j, wheres[i].comparison, wheres[i].searchValue))
+                        toUpdate[j] = 0;
+                    break;
+                case '|':
+                case 'o':
+                case 'O':
                     toUpdate[j] = 1;
-                }
+                case '~':
+                    if(!compare(nameToCol(table, wheres[i].searchColName), j, wheres[i].comparison, wheres[i].searchValue))
+                        toUpdate[j] = 0;
+                    break;
+                default:
+                    printf("Error: Invalid where connective '%c' provided into delete function.", conns[i]);
+                    break;
             }
-            else if(currConnect == '\0') {
-                if(!compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue))
-                    toUpdate[j] = 0;
-            }
-            else {
-                printf("Error: Invalid where connective '%c' provided into update function.", currConnect);
-            }
-            if(i == numConds - 1 && toUpdate[j] == 1) {
-                if(column.type == CHAR)
-                    column.values[j].CHAR = (char*)newValue;
-                else if(column.type == INTEGER)
-                    column.values[j].INTEGER = *((int*)newValue);
-                else if(column.type == DECIMAL)
-                    column.values[j].DECIMAL = *((double*)newValue);
+
+            if(toUpdate[j] == 1) {
+                prevGoodJs = realloc(prevGoodJs, sizeof(int) * (++numPrevGoodJs));
+                prevGoodJs[numPrevGoodJs - 1] = j;
             }
 
         }
-        if(i < numConds - 1) {
-            currConnect = va_arg(ap, int);
+
+        if((i == numWheres - 1 || conns[i + 1] == '|' || conns[i + 1] == 'o' || conns[i + 1] == 'O') && (numPrevGoodJs > 0 && numPrevGoodJs <= table->numRows)) {
+            for(int k = 0; k < numPrevGoodJs; k++) {
+                if(column.type == CHAR)
+                    column.values[prevGoodJs[k]].CHAR = (char*)newValue;
+                else if(column.type == INTEGER)
+                    column.values[prevGoodJs[k]].INTEGER = *((int*)newValue);
+                else if(column.type == DECIMAL)
+                    column.values[prevGoodJs[k]].DECIMAL = *((double*)newValue);
+            }
         }
+        free(prevGoodJs);
     }
 
 }
 
 /**
- * Format: delete(Table* table, int numConds, Where where1, char connective1, Where where2, ...)
+ * Format: delete(Table* table, int numWheres, Where where1, char connective1, Where where2, ...)
  * SQL Origin:
  *  DELETE
  *   FROM tableName
  *   WHERE tableName.whereColName =, >, <, >=, <=, != whereValue
 */
-void delete(Table* table, int numConds, ...) {
-    va_list ap;
-
-    va_start(ap, numConds);
-
-    Where currentWhere;
-    char currConnect = '\0';
+void delete(Table* table, int numWheres, Where* wheres, char* conns) {
     int* prevGoodJs;
     int numPrevGoodJs;
 
@@ -236,28 +239,27 @@ void delete(Table* table, int numConds, ...) {
     for(int i = 0; i < table->numRows; i++)
         toDelete[i] = 1;
 
-    for(int i = 0; i < numConds; i++) {
-        currentWhere = va_arg(ap, Where);
+    for(int i = 0; i < numWheres; i++) {
         prevGoodJs = malloc(sizeof(int));
         numPrevGoodJs = 0;
         for(int j = 0; j < table->numRows; j++) {
-            switch(currConnect) {
+            switch(conns[i]) {
                 case '&':
                 case 'a':
                 case 'A':
-                    if(toDelete[j] == 1 && !compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue))
+                    if(toDelete[j] == 1 && !compare(nameToCol(table, wheres[i].searchColName), j, wheres[i].comparison, wheres[i].searchValue))
                         toDelete[j] = 0;
                     break;
                 case '|':
                 case 'o':
                 case 'O':
                     toDelete[j] = 1;
-                case '\0':
-                    if(!compare(nameToCol(table, currentWhere.searchColName), j, currentWhere.comparison, currentWhere.searchValue))
+                case '~':
+                    if(!compare(nameToCol(table, wheres[i].searchColName), j, wheres[i].comparison, wheres[i].searchValue))
                         toDelete[j] = 0;
                     break;
                 default:
-                    printf("Error: Invalid where connective '%c' provided into delete function.", currConnect);
+                    printf("Error: Invalid where connective '%c' provided into delete function.", conns[i]);
                     break;
             }
 
@@ -267,25 +269,94 @@ void delete(Table* table, int numConds, ...) {
             }
 
         }
-        if(i < numConds - 1)
-            currConnect = va_arg(ap, int);
-
-        if((((currConnect == '|' || currConnect == 'o' || currConnect == 'O')) || (i == numConds - 1)) && (numPrevGoodJs > 0 && numPrevGoodJs <= table->numRows)) {
+        if((i == numWheres - 1 || conns[i + 1] == '|' || conns[i + 1] == 'o' || conns[i + 1] == 'O') && (numPrevGoodJs > 0 && numPrevGoodJs <= table->numRows)) {
             for(int k = 0; k < numPrevGoodJs; k++) {
                 for(int l = 0; l < table->numCols; l++)
-                    table->cols[l].values[prevGoodJs[k]] = table->cols[l].values[table->numRows - 1];
+                    table->cols[l].values[prevGoodJs[k] % (table->numRows - 1)] = table->cols[l].values[table->numRows - 1];
                 table->numRows--;
             }
         }
         free(prevGoodJs);
     }
 
-    if(numConds <= 0)
+    if(numWheres <= 0)
         table->numRows = 0;
 }
 
 void deleteAll(Table* table) {
     table->numRows = 0;
+}
+
+Table select(Table table, Select select, int numWheres, Where* wheres, char* conns) {
+    Table selectedTable;
+
+    selectedTable = selCreate(table, select.numCols, select.colNames);
+
+    delete(&selectedTable, numWheres, notWheres(numWheres, wheres), notConnectives(numWheres - 1, conns));
+
+    return selectedTable;
+}
+
+/**
+ * A create() variant designed for internal use of the select function.
+*/
+Table selCreate(Table baseTable, int numCols, char** colNames) {
+
+    Table newTable;
+
+    newTable.name = strdup(baseTable.name);
+    newTable.cols = malloc(sizeof(Column) * numCols);
+
+    for(int i = 0; i < numCols; i++) {
+        newTable.cols[i] = nameToCol(&baseTable, colNames[i]);
+        newTable.cols[i].name = strdup(colNames[i]);
+        newTable.cols[i].type = nameToCol(&baseTable, colNames[i]).type;
+    }
+
+    newTable.numCols = numCols;
+    newTable.numRows = baseTable.numRows;
+
+    return newTable;
+
+}
+
+Where* notWheres(int numWheres, Where* whereList) {
+    Where* notWheres = malloc(sizeof(Where) * numWheres);
+
+    for(int i = 0; i < numWheres; i++) {
+        notWheres[i].searchColName = strdup(whereList[i].searchColName);
+        notWheres[i].searchValue = malloc(max(sizeof(double), strlen(whereList[i].searchValue)));
+        memcpy(notWheres[i].searchValue, whereList[i].searchValue, max(sizeof(double), strlen(whereList[i].searchValue)));
+        notWheres[i].comparison = oppComp(whereList[i].comparison);
+    }
+    return notWheres;
+}
+
+char* notConnectives(int numConns, char* conns) {
+    char* notConns = malloc(sizeof(char) * (numConns + 2));
+    notConns[0] = '~';
+    for(int i = 1; i < numConns + 1; i++) {
+        switch(conns[i]) {
+            case '&':
+            case 'a':
+            case 'A':
+                notConns[i] = '|';
+                break;
+            case '|':
+            case 'o':
+            case 'O':
+                notConns[i] = '&';
+                break;
+            default:
+                printf("Error: Invalid where connective '%c' provided into delete function.", conns[i]);
+                notConns[i] = conns[i];
+                break;
+        }
+    }
+
+    notConns[numConns + 1] = '\0';
+
+    return notConns;
 }
 
 Column nameToCol(Table* table, char* colName) {
@@ -303,6 +374,14 @@ Column nameToCol(Table* table, char* colName) {
 }
 
 int compare(Column column, int valIndex, char* comparison, void* value) {
+    // printf("Column name: %s, Comparison: %s, Search Value: ", column.name, comparison);
+    // if(column.type == CHAR)
+    //     printf("%s\n", (char*)value);
+    // else if(column.type == INTEGER)
+    //     printf("%d\n", *(int*)value);
+    // else if(column.type == DECIMAL)
+    //     printf("%lf\n", *(double*)value);
+
     if(strcmp(comparison, "=") == 0 || strcmp(comparison, "==") == 0) {
         if(column.type == CHAR)
             return strcmp(column.values[valIndex].CHAR, (char*)value) == 0;
@@ -370,39 +449,136 @@ int compare(Column column, int valIndex, char* comparison, void* value) {
 
 }
 
+char* oppComp(char* comparison) {
+    char* oppComp;
+
+    if(strcmp(comparison, "=") == 0 || strcmp(comparison, "==") == 0)
+        oppComp = "!=";
+    else if(strcmp(comparison, "<") == 0)
+        oppComp = ">=";
+    else if(strcmp(comparison, "<=") == 0)
+        oppComp = ">";
+    else if(strcmp(comparison, ">") == 0)
+        oppComp = "<=";
+    else if(strcmp(comparison, ">=") == 0)
+        oppComp = "<";
+    else if(strcmp(comparison, "<>") == 0 || strcmp(comparison, "!=") == 0)
+        oppComp = "==";
+    else {
+        printf("Error: Invalid comparison entered.\n");
+        oppComp = NULL;
+    }
+
+    return oppComp;
+}
+
 Where newWhere(char* searchColName, char* comparison, void* searchValue) {
     Where newWhere;
     newWhere.searchColName = strdup(searchColName);
     newWhere.comparison = strdup(comparison);
-    // newWhere.searchValue = malloc(sizeof(void*));
-    // memcpy(newWhere.searchValue, searchValue, strlen(searchValue));
-    newWhere.searchValue = searchValue;
+    newWhere.searchValue = malloc(max(sizeof(double), strlen(searchValue)));
+    memcpy(newWhere.searchValue, searchValue, max(sizeof(double), strlen(searchValue)));
+    // newWhere.searchValue = searchValue;
 
     return newWhere;
 }
+Select newSelect(int numCols, char** colNames, char** orderByCols, char** groupByCols, int distinct) {
+    Select select;
+
+    select.numCols = numCols;
+    select.colNames = colNames;
+    select.orderByCols = orderByCols;
+    select.groupByCols = groupByCols;
+    select.distinct = distinct;
+
+    return select;
+}
+char** nameList(int numNames, ...) {
+    va_list ap;
+    char** nameList = malloc(sizeof(char*) * numNames);
+    va_start(ap, numNames);
+
+    for(int i = 0; i < numNames; i++)
+        nameList[i] = strdup(va_arg(ap, char*));
+
+    va_end(ap);
+
+    return nameList;
+}
+Where* whereList(int numWheres, ...) {
+    va_list ap;
+    va_start(ap, numWheres);
+
+    Where* wheres = malloc(sizeof(Where) * numWheres);
+
+    for(int i = 0; i < numWheres; i++)
+        wheres[i] = va_arg(ap, Where);
+
+    va_end(ap);
+
+    return wheres;
+}
+char* connectiveList(int numConns, ...) {
+
+    va_list ap;
+    va_start(ap, numConns);
+
+    char* conns = malloc(sizeof(char) * (numConns + 2));
+    conns[0] = '~';
+
+    for(int i = 1; i < numConns + 1; i++)
+        conns[i] = va_arg(ap, int);
+
+    va_end(ap);
+
+    conns[numConns + 1] = '\0';
+
+    return conns;
+
+}
+// int* typeList(int numTypes, ...) {
+//     va_list ap;
+//     int* typeList = malloc(sizeof(int) * numTypes);
+//     va_start(ap, numTypes);
+
+//     for(int i = 0; i < numTypes; i++)
+//         typeList[i] = va_arg(ap, int);
+
+//     va_end(ap);
+
+//     return typeList;
+// }
 
 void printTable(Table table) {
 
-    printf("\nTABLE:\n\t\t%s\n\nCOLUMNS:\n\t   ", table.name);
+    printf("\nTABLE: %s\n\t  ", table.name);
     char printString[50];
 
     for(int i = 0; i < table.numCols; i++)
-        printf("%-25s", table.cols[i].name);
+        printf("%-20s", table.cols[i].name);
 
     for(int i = 0; i < table.numRows; i++) {
-        printf("\n\t");
+        printf("\n\t  ");
         for(int j = 0; j < table.numCols; j++) {
             if(table.cols[j].type == CHAR) {
-                sprintf(printString, "    [ %s ]", table.cols[j].values[i].CHAR);
-                printf("%-25s", printString);
+                sprintf(printString, "[ %s ]", table.cols[j].values[i].CHAR);
+                if(strlen(printString) > 18) {
+                    printString[18] = '\0';
+                    printString[17] = ']';
+                    printString[16] = ' ';
+                    printString[15] = '.';
+                    printString[14] = '.';
+                    printString[13] = '.';
+                }
+                printf("%-20s", printString);
             }
             else if(table.cols[j].type == INTEGER) {
-                sprintf(printString, "    [ %d ]", table.cols[j].values[i].INTEGER);
-                printf("%-25s", printString);
+                sprintf(printString, "[ %d ]", table.cols[j].values[i].INTEGER);
+                printf("%-20s", printString);
             }
             else if(table.cols[j].type == DECIMAL) {
-                sprintf(printString, "    [ %.3lf ]", table.cols[j].values[i].DECIMAL);
-                printf("%-25s", printString);
+                sprintf(printString, "[ %.3lf ]", table.cols[j].values[i].DECIMAL);
+                printf("%-20s", printString);
             }
         }
     }
--- sqllib.h
+++ sqllib.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#define max(a, b) ((a) > (b) ? (a) : (b))
 
 typedef union {
     char* CHAR;
@@ -28,25 +29,34 @@ typedef struct {
     int numRows;
 } Table;
 
+//Possinly add BETWEEN functionality to this struct? As an allowed comparion?
 typedef struct {
     char* searchColName;
     char* comparison;
-    void* searchValue;
+    void* searchValue; //Can be a SELECT struct. May need searchValueType for this instead of relying on the column's type.
+    //int searchValueType
 } Where;
 
+typedef struct {
+    int numCols;
+    char** colNames; //Can include SUM(col), COUNT(col), AVG(col), MIN(col), MAX(col), AS newCol
+    char** orderByCols; //Can include ASC or DESC
+    char** groupByCols;
+    int distinct; //0 or 1
+} Select;
+
+//Primary and Foreign Keys are something yet to be added.
+//Primary keys use hashtables in SQL, so we could do that here too.
 Table create(char* tableName, int numCols, ...); //Implemented
 //... --> char *col1Name, int col1Type, ...
 
-Table select(char* colName, Table table, int distinct);
-Table selectWhere(char* colName, Table table, int distinct, int numConds, ...);
-//... --> Where where1, char &/|, Where where2, ...
+Table select(Table table, Select select, int numWheres, Where* wheres, char* conns);
+Table selCreate(Table baseTable, int numCols, char** colNames); //Implemented
 
 void insert(Table* table, int numValues, ...); //Implemented
 //... --> char *col1Name, void *value1, ...
-void update(Table* table, char* colName, void* newValue, int numConds, ...); //Implemented
-//... --> Where where1, char* AND/OR, Where where2, ...
-void delete(Table* table, int numConds, ...); //Implemented
-//... --> Where where1, char* AND/OR, Where where2, ...
+void update(Table* table, char* colName, void* newValue, int numWheres, Where* wheres, char* conns);
+void delete(Table* table, int numWheres, Where* wheres, char* conns); //Implemented
 void deleteAll(Table* table); //Implemented
 void freeTable(Table* table);
 
@@ -59,10 +69,15 @@ void exportString(Table table);
 void exportSQL(Table table, char* filename);
 Table importTable(char* filename);
 int compare(Column column, int valIndex, char* comparison, void* value); //Implemented
+char* oppComp(char* comparison); //Implemented
+
 Where newWhere(char* searchColName, char* comparison, void* searchValue); //Implemented
+Select newSelect(int numCols, char** colNames, char** orderByCols, char** groupByCols, int distinct); //Implemented
+char** nameList(int numNames, ...); //Implemented
+Where* whereList(int numWheres, ...); //Implemented
+char* connectiveList(int numConns, ...); //Implemented
+Where* notWheres(int numWheres, Where* whereList); //Implemented
+char* notConnectives(int numConns, char* conns); //Implemented
 
 Table matrixToTable(void** matrix, char* tableName, char** colNames, int type);
 void matrixToSQL(void** matrix, int type, char* filename);
-
-//Perhaps change to add ... parameters to these functions instead of using arrays of elements and columns.
-//Need stdarg.h for this
